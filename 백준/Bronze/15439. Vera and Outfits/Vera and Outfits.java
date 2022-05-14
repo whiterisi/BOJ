44bcb6a39276020541722148e114fd7136505043
MING <97995420+whiterisi@users.noreply.github.com>
2022-05-14 16:20:43 +0900

    [Bronze V] Title: Vera and Outfits, Time: 112 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. Vera and Outfits/Vera and Outfits.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+	static int n;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		System.out.println(n*n-n);
+	}
+}
