ad9c3c55db9baaa55807ec39340d6a550c08acc5
MINJI <97995420+whiterisi@users.noreply.github.com>
2022-10-22 13:26:26 +0900

    [Silver IV] Title: 좋은 단어, Time: 384 ms, Memory: 19008 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int answer=0;
+		for(int i=0; i<N; i++) {
+			String str = sc.next();
+			Stack<Character> stack = new Stack<>();
+			for(int j=0; j<str.length();j++) {
+				char c = str.charAt(j);
+				if(stack.isEmpty()||stack.peek()!=c) {
+					stack.add(c);
+				}else {
+					stack.pop();
+				}
+			}
+			if(stack.isEmpty()) answer++;
+		}
+		System.out.println(answer);
+		
+	}
+}
