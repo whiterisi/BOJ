bf546048ed1566e3e8952fe5b98e9430e4a8ab5f
MINJI <97995420+whiterisi@users.noreply.github.com>
2023-02-14 22:38:55 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.63 ms, Memory: 59.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution{
+    public int solution(String s){
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char c= s.charAt(i);
+            if(stack.isEmpty()){
+                stack.add(c);
+            }else if(stack.peek()==c){
+                stack.pop();
+            }else{
+                stack.add(c);
+            }
+        }
+        if(stack.isEmpty()) answer =1;
+        return answer;
+    }
+}
